e5f53f3eabb180c2465d1a997a0a5505a9726577
Darko Kukovec <darko.kukovec@infinum.hr>
2015-06-24 17:27:27 +0200

    custom before support

--- config/custom.js
+++ config/custom.js
@@ -1,4 +1,5 @@
 module.exports = {
   deps: [],
+  before: [],
   task: function() {}  
 };
--- tasks/custom.js
+++ tasks/custom.js
@@ -1,5 +1,5 @@
 module.exports = function(gulp, H, options) {
-  gulp.task(options.taskName, function(done) {
+  gulp.task(options.taskName, [].concat(options.before), function(done) {
     H.loadDeps([].concat(options.deps));
     return options.task(H, options, done);
   });
